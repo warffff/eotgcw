eda7536d8612453273c487bf1eeff5407ce62eca
warffff <vadik-logaa@yandex.ru>
2026-06-04 23:27:51 +0300

    Delete api/player-link.js

--- api/player-link.js
+++
@@ -1,9 +0,0 @@
-function sendJson(res, status, data) {
-  res.statusCode = status;
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.end(JSON.stringify(data));
-}
-
-module.exports = async function handler(req, res) {
-  return sendJson(res, 410, { ok: false, disabled: true, error: 'Player statistics link is disabled for this project.' });
-};
