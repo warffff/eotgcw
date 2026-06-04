b34e10745dec53f73ca5e02d6d9dd65582e4396c
warffff <vadik-logaa@yandex.ru>
2026-06-05 01:08:01 +0300

    Delete api directory

--- api/holonet/public.js
+++
@@ -1,9 +0,0 @@
-const { getJSON } = require('./_kv')
-module.exports = async (req, res) => {
-  try {
-    const state = await getJSON('eotg:holonet:state', {})
-    res.status(200).json({ ok: true, state })
-  } catch (e) {
-    res.status(500).json({ ok: false, error: String(e.message || e) })
-  }
-}
